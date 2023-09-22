dec9ae83266822b446b187375980d4773369bd9c
Alejandro Munoz Berral <amunoz-b@cbr8s5.42barcelona.com>
2023-09-22 14:11:10 +0200

    Prueba2

---
+++ Prueba2/ft_printf.h
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ft_putchar(int c);
+int ft_printf(char const *str, ...);
+void comprueba_tipo(char *str, va_list ap);
+void ft_puthex_long(unsigned long numero);
+void ft_puthex_min(unsigned int numero);
+void ft_puthex_may(unsigned int numero);
+void filtrohex(unsigned int numero, int flag);
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
